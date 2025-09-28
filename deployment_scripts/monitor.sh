06ee4f3f2c25c5028c9b14563a9119a23a4c7301
Petzys <87223648+Petzys@users.noreply.github.com>
2025-09-28 14:52:50 -0400

    added monitoring and red team script

--- deployment_scripts/monitor.sh
+++ deployment_scripts/monitor.sh
@@ -1,28 +1,32 @@
-# !/bin/bash
+#!/bin/bash
 
-# Make sure that the script is run as root
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root"
-    exit 1
-fi
-
-# Make sure that parameter is passed and that /root/.ssh/id_ed25519 exists
+# Check arguments
 if [ -z "$1" ]; then
     echo "No HF_TOKEN provided. Please provide the token as the first argument."
     exit 1
 fi
-if [ ! -f /root/.ssh/id_ed25519 ]; then
-    echo "SSH private key /root/.ssh/id_ed25519 not found. Please ensure it exists."
+
+if [ -z "$2" ]; then
+    echo "No SSH private key path provided. Please provide it as the second argument."
+    exit 1
+fi
+
+HF_TOKEN="$1"
+KEY_PATH="$2"
+
+# Check if the provided key exists
+if [ ! -f "$KEY_PATH" ]; then
+    echo "SSH private key $KEY_PATH not found. Please ensure it exists."
     exit 1
 fi
 
 # Monitor if the application is running and append result to log file
-curl -sI http://paffenroth-23.dyn.wpi.edu:8011 | grep "200 OK" >> /var/log/xkcd_finder_monitor.log
+curl -sI http://paffenroth-23.dyn.wpi.edu:8011 | grep "200 OK" >> ~/xkcd_finder_monitor.log
 if [ $? -eq 0 ]; then
-    echo "$(date): Application is running." >> /var/log/xkcd_finder_monitor.log
+    echo "$(date): Application is running." >> ~/xkcd_finder_monitor.log
 else
-    echo "$(date): Application is NOT running." >> /var/log/xkcd_finder_monitor.log
+    echo "$(date): Application is NOT running." >> ~/xkcd_finder_monitor.log
     # Log in via SSH and rerun deploy.sh by pulling it with curl and executing directly
-    ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no -p 22011 student-admin@paffenroth-23.dyn.wpi.edu \
-        "curl -fsSL https://raw.githubusercontent.com/Badrivishal/xkcd_finder/refs/heads/deployment/deployment_scripts/deploy.sh | sudo bash -s -- $1"
+    ssh -i "$KEY_PATH" -o StrictHostKeyChecking=no -p 22011 student-admin@paffenroth-23.dyn.wpi.edu \
+        "curl -fsSL https://github.com/Badrivishal/xkcd_finder/raw/refs/heads/main/deployment_scripts/deploy.sh | sudo bash -s -- $HF_TOKEN"
 fi
