531cc62e0fb63e3550e52b0b445c1e1843d15074
Petzys <87223648+Petzys@users.noreply.github.com>
2025-11-21 17:48:48 -0500

    Fix: docker io link in gcp script

--- cloud_deploy/gcp.sh
+++ cloud_deploy/gcp.sh
@@ -2,7 +2,7 @@ gcloud run deploy xkcd-finder \
     --set-env-vars=HF_TOKEN=$1 \
     --memory=2Gi \
     --startup-probe=timeoutSeconds=240,periodSeconds=240,tcpSocket.port=7860 \
-    --image mrpetzi/xkcd_finder \
+    --image docker.io/mrpetzi/xkcd_finder:latest \
     --platform managed \
     --region us-central1 \
     --allow-unauthenticated \
