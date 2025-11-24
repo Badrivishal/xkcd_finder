4259ce7857cb2a1686ad4a5a54fda8739b993c14
Petzys <87223648+Petzys@users.noreply.github.com>
2025-11-24 16:15:37 -0500

    update docker hub link

--- cloud_deploy/gcp.sh
+++ cloud_deploy/gcp.sh
@@ -2,7 +2,7 @@ gcloud run deploy xkcd-finder \
     --set-env-vars=HF_TOKEN=$1 \
     --memory=2Gi \
     --startup-probe=timeoutSeconds=240,periodSeconds=240,tcpSocket.port=7860 \
-    --image docker.io/mrpetzi/xkcd_finder:latest \
+    --image docker.io/badrivishalp/xkcd_finder:latest \
     --platform managed \
     --region us-central1 \
     --allow-unauthenticated \
